694702da110741ddc57cc6d28512c6e660fbc4df
Zachary Buresh <zacburesh@gmail.com>
2020-02-10 21:07:27 -0600

    Implemented bottom navigation view and custom toolbar.

--- app/src/main/java/com/example/server_score/MainActivity.kt
+++ app/src/main/java/com/example/server_score/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.server_score
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bt_greet.setOnClickListener{
-            tv_name.text = "Zac"
-        }
+        toolbar = findViewById(R.id.toolbar_score)
+        setSupportActionBar(toolbar)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
     }
 
 }
