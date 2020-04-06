3ba683f1c2511bd6cc6474d37a160dae434fb5fd
Zachary Buresh <zacburesh@gmail.com>
2020-04-06 12:52:37 -0500

    Converted Score Tab to MVP Architecture.

--- app/src/main/java/com/example/server_score/score/ScoreActivity.kt
+++ app/src/main/java/com/example/server_score/score/ScoreActivity.kt
@@ -3,30 +3,24 @@ package com.example.server_score.score
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.room.Room
 import com.example.server_score.MainActivity
 import com.example.server_score.R
 import com.example.server_score.add.AddActivity
-import com.example.server_score.model.AppDatabase
-import com.example.server_score.model.Shifts
 import com.example.server_score.model.Users
 import com.example.server_score.predictions.PredictionsActivity
 import kotlinx.android.synthetic.main.activity_add.navigationView
 import kotlinx.android.synthetic.main.activity_score.*
-import kotlinx.coroutines.runBlocking
 import java.lang.Integer.parseInt
-import java.math.RoundingMode
-import java.text.DecimalFormat
 
 
-class ScoreActivity : AppCompatActivity() {
+class ScoreActivity : ScoreInterface, AppCompatActivity(){
 
     lateinit var toolbar: Toolbar
+    private lateinit var updatedUser: Users
 
     companion object{
         const val STANDARD_HOURLY_AVG = 20.0
@@ -39,77 +33,10 @@ class ScoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
 
-        toolbar = findViewById(R.id.toolbar_score)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setDisplayShowHomeEnabled(true)
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp)
-        toolbar.setNavigationOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        val toolbarTitle: TextView = toolbar.findViewById(R.id.toolbar_title)
-        val username = intent.getStringExtra("USERNAME")
-        toolbarTitle.text = username
-        val item: MenuItem = navigationView.menu.getItem(0)
-        item.isChecked = true
-        slider.min = 0
-        slider.max = 40
+        val username = setupActivity()
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "Server-Score-Database"
-        ).build()
-
-        var shiftCount = 0
-        var tipTotal = 0F
-        var salesTotal = 0F
-        var hoursTotal = 0F
-        var addOnsTotal = 0F
-        var checkTimeTotal = 0
-        getShifts(db).forEach() {
-            if (it.name == username) {
-                shiftCount++
-                tipTotal += it.totalTips!!
-                salesTotal += it.totalSales!!
-                hoursTotal += it.hours!!
-                addOnsTotal += it.addOns!!
-                checkTimeTotal += it.checkTime!!
-            }
-        }
-
-        var userShiftCount = 0
-        var userId = 0
-        var ct = 0
-        getUsers(db).forEach() {
-            userId++
-            if (it.name == username && ct == 0) {
-                ct++
-                userShiftCount++
-                if (userShiftCount == 0 || shiftCount == 0) {
-                    tv_avg_tip_num.text = "0"
-                    tv_sales_num.text = "0"
-                    tv_wage_num.text = "0"
-                    tv_add_ons_num.text = "0"
-                    tv_check_time_num.text = "0"
-                } else {
-                    val updatedUser = Users(
-                        it.uid, username, tipTotal / shiftCount, salesTotal / shiftCount,
-                        tipTotal / hoursTotal, addOnsTotal / shiftCount,
-                        checkTimeTotal / shiftCount
-                    )
-                    updateUser(db, updatedUser)
-                    tv_avg_tip_num.text = "$" + String.format("%.2f", updatedUser.avgTips)
-                    tv_sales_num.text = String.format("%.2f", ((updatedUser.avgTips?.div(updatedUser.avgSales!!))?.times(100))) + "%"
-                    tv_wage_num.text = "$" + String.format("%.2f", updatedUser.avgHourly)
-                    tv_add_ons_num.text = "$" + String.format("%.2f", updatedUser.avgAddOns)
-                    tv_check_time_num.text = updatedUser.avgCheckTime.toString() + " Minutes"
-                }
-            }
-        }
-
-        calculateServerScore(db, username)
+        val presenter = ScorePresenter(this, this)
+        presenter.startPresenter(username)
 
         navigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -133,34 +60,50 @@ class ScoreActivity : AppCompatActivity() {
         }
     }
 
-    fun getShifts(db: AppDatabase): List<Shifts> = runBlocking {
-        db.shiftDao().getAllShifts()
+    override fun initializeTextViews() {
+        tv_avg_tip_num.text = "0"
+        tv_sales_num.text = "0"
+        tv_wage_num.text = "0"
+        tv_add_ons_num.text = "0"
+        tv_check_time_num.text = "0"
     }
 
-    fun getUsers(db: AppDatabase): List<Users> = runBlocking {
-        db.userDao().getAllUsers()
+    @SuppressLint("SetTextI18n")
+    override fun setTextViews(updatedUser: Users){
+        System.out.println(updatedUser)
+        tv_avg_tip_num.text = "$" + String.format("%.2f", updatedUser.avgTips)
+        tv_sales_num.text = String.format("%.2f", ((updatedUser.avgTips?.div(updatedUser.avgSales!!))?.times(100))) + "%"
+        tv_wage_num.text = "$" + String.format("%.2f", updatedUser.avgHourly)
+        tv_add_ons_num.text = "$" + String.format("%.2f", updatedUser.avgAddOns)
+        tv_check_time_num.text = updatedUser.avgCheckTime.toString() + " Minutes"
     }
 
-    fun updateUser(db: AppDatabase, updatedUser: Users) = runBlocking {
-        db.userDao().update(updatedUser)
+    override fun initializeScore(){
+        tv_score.text = "0"
     }
 
-    fun calculateServerScore(db: AppDatabase, username: String){
-        getUsers(db).forEach(){
-            if(it.name == username){
-                if(it.avgHourly == null || it.avgCheckTime == null || it.avgAddOns == null || it.avgTips == null){
-                    tv_score.text = "0"
-                }
-                else {
-                    val hourlyResult = it.avgHourly - STANDARD_HOURLY_AVG
-                    val addonResult = it.avgAddOns - STANDARD_ADD_ON_AVG
-                    val checktimeResult = STANDARD_CHECK_TIME_AVG - it.avgCheckTime
-                    val serverScore =
-                        String.format("%.0f", hourlyResult + addonResult + checktimeResult)
-                    tv_score.text = serverScore
-                    slider.progress = parseInt(serverScore) + 20
-                }
-            }
+    override fun setServerScore(serverScore: String) {
+        tv_score.text = serverScore
+        slider.progress = parseInt(serverScore) + 20
+    }
+
+    private fun setupActivity(): String{
+        toolbar = findViewById(R.id.toolbar_score)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp)
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+        val toolbarTitle: TextView = toolbar.findViewById(R.id.toolbar_title)
+        val username = intent.getStringExtra("USERNAME")
+        toolbarTitle.text = username
+        val item: MenuItem = navigationView.menu.getItem(0)
+        item.isChecked = true
+        slider.max = 40
+        return username
     }
 }
